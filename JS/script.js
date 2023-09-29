8bfed7b634a497b1670e6491ccf7d1bef4d56b70
pietrolupi <pietro.lupi92@gmail.com>
2023-09-29 15:49:28 +0200

    [add] stampa dinamica corretta

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,29 @@
 
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      toDo: [
+        
+      ],
+
+      textInsert: '',
+
+  
+
+    }
+  },
+
+  methods: {
+    addList(string){
+      this.toDo.push({text: string, done: false})
+      console.log(this.toDo);
+      this.textInsert = '';
+    }
+  },
+
+  mounted(){
+   
+  }
+}).mount('#app')
