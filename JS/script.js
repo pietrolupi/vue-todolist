f624f8bff93f567aa43ae6180d9ccd0558517a3e
pietrolupi <pietro.lupi92@gmail.com>
2023-10-01 11:39:20 +0200

    [add]: aggiunte tutte le funzionalità bonus

--- JS/script.js
+++ JS/script.js
@@ -10,16 +10,37 @@ createApp({
 
       textInsert: '',
 
-  
+      lengthControl: false
 
     }
   },
 
   methods: {
     addList(string){
-      this.toDo.push({text: string, done: false})
-      console.log(this.toDo);
-      this.textInsert = '';
+      if(this.textInsert.length < 5){
+        this.lengthControl = true;
+        console.log(this.lengthControl);
+      }else{
+
+        this.toDo.push({text: string, done: false})
+        console.log(this.toDo);
+        this.textInsert = '';
+        this.lengthControl = false;
+      }
+    },
+
+    completeTask(index){
+      console.log('cliccatoooo')
+      this.toDo[index].done = !this.toDo[index].done;
+      console.log(this.toDo[index].done);
+    },
+
+    removeTask(index){
+      if(this.toDo[index].done){
+        this.toDo.splice(index,1)
+      }else{
+        alert('WARNING: click on a task to mark it as "completed" before removing it!')
+      }
     }
   },
 
