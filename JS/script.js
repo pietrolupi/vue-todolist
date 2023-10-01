4d133e7734f45ef3b6a0d97a301c0c59ed0b1a0a
pietrolupi <pietro.lupi92@gmail.com>
2023-10-01 16:04:36 +0200

    [add]: funzione cronometro per ogni opzione!!!

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,23 @@ createApp({
 
       textInsert: '',
 
-      lengthControl: false
+      lengthControl: false,
+      optionControl: false,
+
+      isStarted: false,
+      clock: null,
+      minuti: 0,
+      secondi: 0,
+      millisecondi: 0,
+
+      selectedTime: 'Select the available time:',
+      
+      timeList: [],
+
+      clockList: [],
+
+      overtimeList: [],
+      completedList:[]
 
     }
   },
@@ -18,30 +34,104 @@ createApp({
   methods: {
     addList(string){
       if(this.textInsert.length < 5){
+        this.optionControl = false;
         this.lengthControl = true;
-        console.log(this.lengthControl);
-      }else{
+        
+      }else if(this.selectedTime === 'Select the available time:'){
+        this.lengthControl = false;
+        this.optionControl = true;
+        
+      } else{
 
         this.toDo.push({text: string, done: false})
-        console.log(this.toDo);
+        
+        
         this.textInsert = '';
         this.lengthControl = false;
+        this.optionControl = false;
+
+        this.timeList.push(this.selectedTime);
+
+        this.overtimeList.push({overtime:false, done: false})
+
+        this.clockList.push({
+          isStarted: false,
+          clock: null,
+          minuti: 0,
+          secondi: 0,
+          millisecondi: 0})
+
+        this.selectedTime = 'Select the available time:'
+
+        console.log(this.clockList)
       }
     },
 
     completeTask(index){
-      console.log('cliccatoooo')
+      
       this.toDo[index].done = !this.toDo[index].done;
-      console.log(this.toDo[index].done);
+      this.overtimeList[index].done = !this.overtimeList[index].done
     },
 
     removeTask(index){
       if(this.toDo[index].done){
-        this.toDo.splice(index,1)
+        this.toDo.splice(index,1);
+        this.timeList.splice(index,1);
+        this.overtimeList.splice(index,1);
+        this.clockList.splice(index,1);
       }else{
         alert('WARNING: click on a task to mark it as "completed" before removing it!')
       }
+    },
+
+    startChrono(index) {
+      this.clockList[index].isStarted = true;
+      console.log(this.clockList);
+      this.clockList[index].clock = setInterval ( () => {
+        this.clockList[index].millisecondi += 1
+        
+        
+
+        if(this.clockList[index].millisecondi === 100) {
+          this.clockList[index].millisecondi = 0;
+          this.clockList[index].secondi++
+        }
+        if (this.clockList[index].secondi === 60) {
+          this.clockList[index].secondi = 0;
+          this.clockList[index].minuti++
+        }else if(this.clockList[index].secondi == this.timeList[index] && this.clockList[index].secondi){
+          
+          this.pauseChrono(index)
+          this.overtimeList[index].overtime = true;
+        }
+      
+      }, 10)
+    },
+
+    resetChrono (index) {
+      this.pauseChrono(index);
+      this.clockList[index].minuti = 0;
+      this.clockList[index].secondi = 0;
+      
+    },
+
+    pauseChrono(index) {
+      this.clockList[index].isStarted = false;
+      clearInterval(this.clockList[index].clock);
+    },
+
+   
+  },
+
+  computed: {
+
+    secondiZerofill (index) {
+      return this.clockList[index].secondi.toString().padStart(2, '0')
+    },
+    minutiZerofill (index) {
+      return this.clockList[index].minuti.toString().padStart(2, '0')
     }
+      
   },
 
   mounted(){
